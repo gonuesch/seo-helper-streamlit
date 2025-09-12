6170c771df68bf2cc475e34353a3b1b5c5772d7d
Gordon Nüsch <gordon.nuesch@rowohlt.de>
2025-09-12 13:52:22 +0200

    Fix NameError: get_tts_credentials function call
    - Remove unnecessary tts_credentials call in main app initialization
    - TTS credentials are loaded when needed in TTS functions
    - Fixes NameError that was preventing app startup

--- seo_app.py
+++ seo_app.py
@@ -136,7 +136,7 @@ if 'GOOGLE_APPLICATION_CREDENTIALS' in os.environ:
     logger.info("🗑️ GOOGLE_APPLICATION_CREDENTIALS Umgebungsvariable entfernt")
 
 # Lade Credentials
-tts_credentials = get_tts_credentials()
+# TTS credentials will be loaded when needed in TTS functions
 
 # Richte den Google Cloud Pub/Sub Publisher ein
 # Verwende immer Cloud Run Default Service Account für Pub/Sub
